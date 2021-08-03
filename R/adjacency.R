9d2754aa90081d204218b123be5ed710f3771fc7
Valentin Courgeau <val.courgeau@gmail.com>
2021-08-03 14:38:59 +0200

    fix: correct spelling

--- R/adjacency.R
+++ R/adjacency.R
@@ -1,60 +1,77 @@
-# This script attempts to provide construction functions for 4 main types of networks;
+# This script attempts to provide construction functions for
+# 4 main types of networks;
 # They cover the following properties
 #   - max(degree) = 0, 2, 4/8, d-1
 
 #' Checks that \code{|theta_1| < theta_2}.
 #' @param theta_1 Off-diagonal value or network effect.
 #' @param theta_2 Diagonal value or momentum effect.
-CheckThetas <- function(theta_1, theta_2){
-  assertthat::assert_that(theta_2 > abs(theta_1), msg = paste('In', match.call()[1], 'must verify theta_2 > abs(theta_1)'))
+check_thetas <- function(theta_1, theta_2) {
+  assertthat::assert_that(
+    theta_2 > abs(theta_1),
+    msg = paste("In", match.call()[1], "must verify theta_2 > abs(theta_1)")
+  )
 }
 
-#' Creates adjacency matrix with max degree zewro.
+#' Creates adjacency matrix with max degree zero.
 #' @param d Number of nodes.
 #' @param theta_1 Off-diagonal value or network effect.
 #' @param theta_2 Diagonal value or momentum effect.
 #' @return adjacency matrix of a graph with max degree zero.
-#' @example IsolatedNetwork(d=10)
+#' @example isolated_network(d=10)
 #' @export
-IsolatedNetwork <- function(d, theta_1, theta_2){
+isolated_network <- function(d, theta_1, theta_2) {
   # theta_2 is the diagonal element
-  return(theta_2*diag(d))
+  return(theta_2 * diag(d))
 }
 
 #' Creates adjacency matrix with min degree one and max degree two.
 #' @param d Number of nodes.
 #' @param theta_1 Off-diagonal value or network effect.
 #' @param theta_2 Diagonal value or momentum effect.
-#' @param directed Boolean if lower triangular is the opposite to the upper triangular matrix.
+#' @param directed Boolean if lower triangular is the opposite
+#'     to the upper triangular matrix.
 #' @return Adjacency matrix of a graph with max degree two and minimum one.
-#' @example PolymerNetwork(d=10)
+#' @example polymer_network(d=10)
 #' @export
-PolymerNetwork <- function(d, theta_1, theta_2, directed=F){
-  CheckThetas(theta_1, theta_2)
+polymer_network <- function(d, theta_1, theta_2, directed = F) {
+  check_thetas(theta_1, theta_2)
   # theta_2 is the diagonal element
-  mat_temp <- AugmentedDiag(d = d, offset = 1) + if(directed){-AugmentedDiag(d = d, offset = 1)} else{AugmentedDiag(d=d,offset=-1)}
+  mat_temp <- AugmentedDiag(d = d, offset = 1) + if (directed) {
+    -AugmentedDiag(d = d, offset = 1)
+  } else {
+    AugmentedDiag(d = d, offset = -1)
+  }
   mat_temp <- theta_1 * mat_temp / rowSums(abs(mat_temp))
-  return(mat_temp + IsolatedNetwork(d = d, theta_2 = theta_2))
+  return(mat_temp + isolated_network(d = d, theta_2 = theta_2))
 }
 
 #' Lattice/grid-like network with max degree of four across all nodes.
 #' @param d Number of nodes.
 #' @param theta_1 Off-diagonal value or network effect.
 #' @param theta_2 Diagonal value or momentum effect.
-#' @param directed Boolean if lower triangular is the opposite to the upper triangular matrix.
-#' @example LatticeNetwork(10, theta_1 = 1, theta_2 = 2)
+#' @param directed Boolean if lower triangular is the opposite
+#'     to the upper triangular matrix.
+#' @example lattice_network(10, theta_1 = 1, theta_2 = 2)
 #' @importFrom copCAR adjacency.matrix
 #' @export
-LatticeNetwork <- function(d, theta_1, theta_2, directed=F){
+lattice_network <- function(d, theta_1, theta_2, directed = F) {
   # TODO add directed for it
-  CheckThetas(theta_1, theta_2)
+  check_thetas(theta_1, theta_2)
 
-  if(directed) stop('NotImplementedError')
+  if (directed) stop("NotImplementedError")
   d_real <- round(sqrt(d))
   net_matrix <- adjacency.matrix(d_real)
-  if(d_real^2 < d){
-    net_matrix <- rbind(net_matrix, matrix(0, nrow=d-d_real^2, ncol=d_real*d_real))
-    net_matrix <- cbind(net_matrix, rbind(matrix(0, ncol=d-d_real^2, nrow=d_real*d_real), diag(1, nrow = d - d_real^2)))
+  if (d_real^2 < d) {
+    net_matrix <- rbind(
+      net_matrix,
+      matrix(0, nrow = d - d_real^2, ncol = d_real * d_real)
+    )
+    net_mat_tmp <- rbind(
+      matrix(0, ncol = d - d_real^2, nrow = d_real * d_real),
+      diag(1, nrow = d - d_real^2)
+    )
+    net_matrix <- cbind(net_matrix, net_mat_tmp)
   }
 
   net_matrix <- theta_1 * RowNormalised(net_matrix)
@@ -67,14 +84,15 @@ LatticeNetwork <- function(d, theta_1, theta_2, directed=F){
 #' @param d Number of nodes.
 #' @param theta_1 Off-diagonal value or network effect.
 #' @param theta_2 Diagonal value or momentum effect.
-#' @param directed Boolean if lower triangular is the opposite to the upper triangular matrix.
-#' @example FullyConnectedNetwork(10, 1, 2)
+#' @param directed Boolean if lower triangular is the opposite
+#'     to the upper triangular matrix.
+#' @example fully_connected_network(10, 1, 2)
 #' @export
-FullyConnectedNetwork <- function(d, theta_1, theta_2, directed=F){
+fully_connected_network <- function(d, theta_1, theta_2, directed = F) {
   net_temp <- matrix(1, d, d)
   net_temp <- theta_1 * RowNormalised(net_temp)
 
-  if(directed){
+  if (directed) {
     net_temp[lower.tri(net_temp)] <- -net_temp[lower.tri(net_temp)]
   }
 
--- tests/testthat/test-adjacency.R
+++ tests/testthat/test-adjacency.R
@@ -1,50 +1,50 @@
-testthat::test_that("Adjacency_IsolatedNetwork__shapes", {
+testthat::test_that("Adjacency_isolated_network__shapes", {
   d <- 10
   theta_2 <- 2
-  adj <- IsolatedNetwork(d = d, theta_2 = theta_2)
+  adj <- isolated_network(d = d, theta_2 = theta_2)
   testthat::expect_equal(dim(adj), c(d, d))
 })
 
-testthat::test_that("Adjacency_IsolatedNetwork__values", {
+testthat::test_that("Adjacency_isolated_network__values", {
   d <- 10
   theta_2 <- 2
-  adj <- IsolatedNetwork(d = d, theta_2 = theta_2)
+  adj <- isolated_network(d = d, theta_2 = theta_2)
   testthat::expect_equal(sum(abs(diag(adj))), d * theta_2)
   diag(adj) <- 0
   testthat::expect_equal(sum(abs(adj)), 0)
 })
 
-testthat::test_that("Adjacency_PolymerNetwork__shapes", {
+testthat::test_that("Adjacency_polymer_network__shapes", {
   d <- 10
   theta_1 <- 1
   theta_2 <- 2
-  adj <- PolymerNetwork(d = d, theta_1 = theta_1, theta_2 = theta_2)
+  adj <- polymer_network(d = d, theta_1 = theta_1, theta_2 = theta_2)
   testthat::expect_equal(dim(adj), c(d, d))
 })
 
-testthat::test_that("Adjacency_PolymerNetwork__values", {
+testthat::test_that("Adjacency_polymer_network__values", {
   d <- 10
   theta_1 <- 1.5
   theta_2 <- 2
-  adj <- PolymerNetwork(d = d, theta_1 = theta_1, theta_2 = theta_2)
+  adj <- polymer_network(d = d, theta_1 = theta_1, theta_2 = theta_2)
   testthat::expect_equal(sum(abs(diag(adj))), d * theta_2)
   diag(adj) <- 0
   testthat::expect_equal(sum(abs(adj)), d * theta_1)
 })
 
-testthat::test_that("Adjacency_LatticeNetwork__shapes_values", {
+testthat::test_that("Adjacency_lattice_network__shapes_values", {
   d <- 10
   theta_1 <- 1.2
   theta_2 <- 2
-  adj <- LatticeNetwork(d = d, theta_1 = theta_1, theta_2 = theta_2)
+  adj <- lattice_network(d = d, theta_1 = theta_1, theta_2 = theta_2)
   testthat::expect_equal(dim(adj), c(d, d))
 })
 
-testthat::test_that("Adjacency_LatticeNetwork__values", {
+testthat::test_that("Adjacency_lattice_network__values", {
   d <- 10
   theta_1 <- 1.2
   theta_2 <- 2
-  adj <- LatticeNetwork(d = d, theta_1 = theta_1, theta_2 = theta_2)
+  adj <- lattice_network(d = d, theta_1 = theta_1, theta_2 = theta_2)
   testthat::expect_equal(sum(abs(diag(adj))), d * theta_2)
   diag(adj) <- 0
   testthat::expect_equal(
